4c802a7fe4235d042832f79086b4caba5d5f6a03
nikolaokonesh <nikolaokonesh@gmail.com>
2019-12-02 21:33:03 +1100

    boom

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,7 @@ module Sklife
     config.time_zone = 'Srednekolymsk'
     config.i18n.default_locale = :ru
     config.i18n.fallbacks = true
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
