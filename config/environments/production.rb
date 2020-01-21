5ccdb388700fb71222e9b515dfd7a01396751b9c
nikolaokonesh <nikolaokonesh@gmail.com>
2020-01-22 10:39:28 +1100

    webp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,8 @@ Rails.application.configure do
     password: Rails.application.credentials.dig(:yandex, :pass),
     domain: config.app_domain
   }
-  config.hosts << ".sklife.ru"
+  config.hosts << /[a-z0-9]+\.sklife\.ru/
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -35,7 +36,9 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.public_file_server.enabled = true
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
