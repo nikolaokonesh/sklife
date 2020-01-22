7128f0e7bf0f3e88599e418087a26b0c39be019f
nikolaokonesh <nikolaokonesh@gmail.com>
2020-01-22 12:40:41 +1100

    slao2

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -5,12 +5,12 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
+  policy.default_src :none
+  policy.font_src    :none
+  policy.img_src     :none
   policy.object_src  :none
-  policy.script_src  :self, :https
-  policy.style_src   :self, :https
+  policy.script_src  :none
+  policy.style_src   :none
   # If you are using webpack-dev-server then specify webpack-dev-server host
   policy.connect_src :self, :https, "http://localhost:3035", "ws://localhost:3035" if Rails.env.development?
 
