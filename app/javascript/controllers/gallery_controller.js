3e125e57571386ecc38712cf19958066b813bf88
nikolaokonesh <nikolaokonesh@gmail.com>
2020-01-22 17:01:21 +1100

    slao9

--- app/javascript/controllers/gallery_controller.js
+++ app/javascript/controllers/gallery_controller.js
@@ -16,12 +16,12 @@ export default class extends Controller {
     const galleryWrapper = document.querySelector('.pswp')
 
     var options = {
-      // shareEl: false,
-      // captionEl: true,
-      // barsSize: {top:0, bottom:0},
-      // bgOpacity: 0.85,
-      // tapToClose: true,
-      // fullscreenEl: false,
+      shareEl: false,
+      captionEl: true,
+      barsSize: {top:0, bottom:0},
+      bgOpacity: 0.85,
+      tapToClose: true,
+      fullscreenEl: false,
       // we don't want browser history for or example for the sake of simplicity
       history: false,
       // and I'm assuming we have unique links in each gallery
