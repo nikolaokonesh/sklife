2616c0e30c5ccfce7cd54d5ff3e74bcd78485696
nikolaokonesh <nikolaokonesh@gmail.com>
2020-01-27 21:13:32 +1100

    optimisation image bg

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,7 +1,7 @@
 module AvatarHelper
   def user_avatar(user, size = 40)
     if user.profile.avatar.attached?
-      url_for(user.profile.avatar.variant(resize_to_fill: [size, size], quality: 70))
+      url_for(user.profile.avatar.variant(resize_to_fill: [size, size], extent: "#{size}x#{size}", gravity: 'Center', quality: 80))
     else
       gravatar_image_url(user.email, size: size)
     end
