20c1f59e0e7060014ea90e120da16c3758e8f0ed
nikolaokonesh <nikolaokonesh@gmail.com>
2020-01-27 16:40:43 +1100

    active storage quality 50 percent

--- app/helpers/avatar_helper.rb
+++ app/helpers/avatar_helper.rb
@@ -1,7 +1,7 @@
 module AvatarHelper
   def user_avatar(user, size = 40)
     if user.profile.avatar.attached?
-      url_for(user.profile.avatar.variant(resize_to_fill: [size, size]))
+      url_for(user.profile.avatar.variant(resize_to_fill: [size, size], quality: 50))
     else
       gravatar_image_url(user.email, size: size)
     end
@@ -18,7 +18,7 @@ module AvatarHelper
 
     html = <<-HTML
     <span class="h-10 w-10 flex cursor-pointer shadow-lg mr-1"
-    style="background: url('#{url_for(user.profile.fon.variant(resize_to_fill: [size, size]))}')
+    style="background: url('#{url_for(user.profile.fon.variant(resize_to_fill: [size, size], quality: 50))}')
       no-repeat center;">
     </span>
     HTML
--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -18,14 +18,14 @@ module ProfilesHelper
 
   def user_fon_attached(user_agent)
     "background: radial-gradient(at top, #{user_agent.profile.bgcolor}4f, #0b0b0be0),
-      url('#{main_app.url_for(user_agent.profile.fon.variant(resize_to_fit: [800, 800], blur: '0x3'))}')
+      url('#{main_app.url_for(user_agent.profile.fon.variant(resize_to_fit: [800, 800], blur: '0x3', quality: 50))}')
       no-repeat center / cover;
       border-top: 8px solid #{user_agent.profile.bgcolor}50;"
   end
 
   def admin_fon_attached
     "background: radial-gradient(at bottom, #{fonadmin.profile.bgcolor}4f, #0b0b0be0),
-      url('#{main_app.url_for(fonadmin.profile.fon.variant(resize_to_fit: [800, 800], blur: '0x3'))}')
+      url('#{main_app.url_for(fonadmin.profile.fon.variant(resize_to_fit: [800, 800], blur: '0x3', quality: 50))}')
       no-repeat center / cover;
       border-top: 8px solid #{fonadmin.profile.bgcolor}50;"
   end
