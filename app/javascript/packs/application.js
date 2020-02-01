8ac6865f1087cedae85393aeed4e67d505127109
nikolaokonesh <nikolaokonesh@gmail.com>
2020-02-02 00:18:58 +1100

    fir ri

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,10 @@ require("@rails/activestorage").start()
 // require("channels")
 window.Rails = Rails
 
+import "trix/dist/trix.css";
 require("stylesheets/application.scss")
-
+import "photoswipe/src/css/main.scss";
+import "photoswipe/src/css/default-skin/default-skin.scss";
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
