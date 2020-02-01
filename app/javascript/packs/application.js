d1c11ab9a579e7357badc3dc71b6822b8efd259f
nikolaokonesh <nikolaokonesh@gmail.com>
2020-02-02 03:08:38 +1100

    slos2

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,12 +9,12 @@ require("@rails/activestorage").start()
 // require("channels")
 window.Rails = Rails
 
-require("trix/dist/trix.css")
+import "trix/dist/trix.css"
 require("trix")
 require("@rails/actiontext")
 require("stylesheets/application.scss")
-require("photoswipe/src/css/main.scss")
-require("photoswipe/src/css/default-skin/default-skin.scss")
+import "photoswipe/src/css/main.scss"
+import "photoswipe/src/css/default-skin/default-skin.scss"
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
