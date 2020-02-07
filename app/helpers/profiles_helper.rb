d77537d5f0b6fa875f7cf08ab9b4b895abcbf770
nikolaokonesh <nikolaokonesh@gmail.com>
2020-02-08 03:11:32 +1100

    ddw

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -29,7 +29,7 @@ module ProfilesHelper
   def admin_fon_attached
     gradi = content_tag(:div, nil, style: "background: radial-gradient(at bottom, #{fonadmin.profile.bgcolor}4f, #0b0b0be0);
                                   border-top: 8px solid #{fonadmin.profile.bgcolor}50;", class: 'absolute w-full max-w-5xl h-64')
-    imagi = image_tag(fonadmin.profile.fon.variant(resize_to_fit: [800, 800], blur: '0x3', quality: 50),
+    imagi = image_tag(fonadmin.profile.fon.variant(resize_to_fill: [1000, 500], blur: '0x3', quality: 70),
                                            data: { behavior: "uppy-preview" },
                                            class: 'w-full h-64 object-fit')
     return fon = gradi + imagi
