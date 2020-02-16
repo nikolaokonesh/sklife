e3111ce6518018e3e7e02b26d46da89aa67016eb
nikolaokonesh <nikolaokonesh@gmail.com>
2020-02-16 23:09:12 +1100

    add uppy folder js

--- app/javascript/controllers/loader_controller.js
+++
@@ -1,9 +0,0 @@
-import { Controller } from "stimulus";
-
-export default class extends Controller {
-  static targets = ["hidden"]
-
-  connect() {
-    setTimeout(() => this.hiddenTarget.classList.add('flash-hidden'), 2000 )
-  }
-}
--- app/javascript/controllers/loadernext_controller.js
+++
@@ -1,8 +0,0 @@
-import { Controller } from "stimulus";
-
-export default class extends Controller {
-  next() {
-    var loaderpage = document.querySelector("#hardloaderpage")
-    loaderpage.classList.remove('flash-hidden')
-  }
-}
